81402ca28a5758029a5d724b0fcb6df15886279c
fernandocst03 <fernando.dorantescst@gmail.com>
2023-02-01 14:12:48 -0600

    Add list projects

---
+++ utils/projects.js
@@ -0,0 +1,40 @@
+export const PROJECTS = [
+
+  {
+    title: 'Sitio web de musica',
+    description: 'Stio web de musica en donde muestra información acerca de las canciones mas escuchadas del momento a nivel global y de los paises Mexico, Estados Unidos y España. ',
+    technologies: ['React', 'Next JS', 'API rest', 'CSS', 'HTML'],
+    img: '/appMusica.png'
+  },
+  {
+    title: 'Aplicacion para adivinar un pokemon.',
+    description: 'Aplicacion mobil en donde tienes que adivinar que pokemon es con solo ver la sombra de este mismo personaje, se hizo uso de la pokeapi para recuperar 200 pokemones para irlos mostrando en un orden aleatorio',
+    technologies: ['Swift'],
+    img: '/appPokemon.png'
+  },
+  {
+    title: 'Sitio web peliculas',
+    description: 'Este sitio web muestra las peliculas y series de television que estan en tendencia en la actualidad, ademas puedes buscar pleiculas y series en base a la categoria a la que pertenezcan. Para este sitio se uso la API de themoviedb.',
+    technologies: ['React', 'API rest', 'HTML', 'CSS'],
+    img: '/appPeliculas.png'
+  },
+  {
+    title: 'App de tareas',
+    description: 'Sitio web que te permite crear una lista de tareas, en donde podras eliminar, marcar como completadas y buscar una tarea en especifico',
+    technologies: ['React', 'HTML', 'CSS'],
+    img: '/appTareas.png'
+  },
+  {
+    title: 'Juego piedra, papel o tijera',
+    description: 'App del juego piedra, papel o tijera, en donde juegas contra la maquina, la cual elige una opcion al azar.',
+    technologies: ['JavaScript', 'HTML', 'CSS'],
+    img: '/appJuego.png'
+  },
+  {
+    title: 'Sitio web de testimonios',
+    description: 'Sitio web en donde se muestra una lista de varios testimonios, se realizar con la finalidad de aprender grid en css',
+    technologies: ['HTML', 'CSS'],
+    img: '/appTestimonios.png'
+  }
+
+]
